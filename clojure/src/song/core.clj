1a9f1510e1994301dbc1704b214b40b8a57566fe
thisdotrob <rs@gower.st>
2018-07-11 12:36:43 +0100

    Use newline chars in song

--- clojure/src/song/core.clj
+++ clojure/src/song/core.clj
@@ -4,47 +4,40 @@
 
 (def animals ["fly" "spider" "bird" "cat" "dog" "cow" "horse"])
 
-(def default-song (str "There was an old lady who swallowed a " (first animals) ".
-I don't know why she swallowed a " (first animals) " - perhaps she'll die!
-
-There was an old lady who swallowed a " (second animals) ";
-That wriggled and wiggled and tickled inside her.
-She swallowed the " (second animals) " to catch the fly;
-I don't know why she swallowed a " (first animals) " - perhaps she'll die!
-
-There was an old lady who swallowed a " (nth animals 2) ";
-How absurd to swallow a " (nth animals 2) ".
-She swallowed the " (nth animals 2) " to catch the " (second animals) ",
-She swallowed the " (second animals) " to catch the " (first animals) ";
-I don't know why she swallowed a " (first animals) " - perhaps she'll die!
-
-There was an old lady who swallowed a " (nth animals 3) ";
-Fancy that to swallow a " (nth animals 3) "!
-She swallowed the " (nth animals 3) " to catch the " (nth animals 2) ",
-She swallowed the " (nth animals 2) " to catch the " (second animals) ",
-She swallowed the " (second animals) " to catch the " (first animals) ";
-I don't know why she swallowed a " (first animals) " - perhaps she'll die!
-
-There was an old lady who swallowed a " (nth animals 4) ";
-What a hog, to swallow a " (nth animals 4) "!
-She swallowed the " (nth animals 4) " to catch the " (nth animals 3) ",
-She swallowed the " (nth animals 3) " to catch the " (nth animals 2) ",
-She swallowed the " (nth animals 2) " to catch the " (second animals) ",
-She swallowed the " (second animals) " to catch the " (first animals) ";
-I don't know why she swallowed a " (first animals) " - perhaps she'll die!
-
-There was an old lady who swallowed a " (nth animals 5) ";
-I don't know how she swallowed a " (nth animals 5) "!
-She swallowed the " (nth animals 5) " to catch the " (nth animals 4) ",
-She swallowed the " (nth animals 4) " to catch the " (nth animals 3) ",
-She swallowed the " (nth animals 3) " to catch the " (nth animals 2) ",
-She swallowed the " (nth animals 2) " to catch the " (second animals) ",
-She swallowed the " (second animals) " to catch the " (first animals) ";
-I don't know why she swallowed a " (first animals) " - perhaps she'll die!
-
-There was an old lady who swallowed a " (nth animals 6) "...
-...She's dead, of course!
-"))
+(def default-song (str "There was an old lady who swallowed a " (first animals) ".\n"
+                       "I don't know why she swallowed a " (first animals) " - perhaps she'll die!\n\n"
+                       "There was an old lady who swallowed a " (second animals) ";\n"
+                       "That wriggled and wiggled and tickled inside her.\n"
+                       "She swallowed the " (second animals) " to catch the fly;\n"
+                       "I don't know why she swallowed a " (first animals) " - perhaps she'll die!\n\n"
+                       "There was an old lady who swallowed a " (nth animals 2) ";\n"
+                       "How absurd to swallow a " (nth animals 2) ".\n"
+                       "She swallowed the " (nth animals 2) " to catch the " (second animals) ",\n"
+                       "She swallowed the " (second animals) " to catch the " (first animals) ";\n"
+                       "I don't know why she swallowed a " (first animals) " - perhaps she'll die!\n\n"
+                       "There was an old lady who swallowed a " (nth animals 3) ";\n"
+                       "Fancy that to swallow a " (nth animals 3) "!\n"
+                       "She swallowed the " (nth animals 3) " to catch the " (nth animals 2) ",\n"
+                       "She swallowed the " (nth animals 2) " to catch the " (second animals) ",\n"
+                       "She swallowed the " (second animals) " to catch the " (first animals) ";\n"
+                       "I don't know why she swallowed a " (first animals) " - perhaps she'll die!\n\n"
+                       "There was an old lady who swallowed a " (nth animals 4) ";\n"
+                       "What a hog, to swallow a " (nth animals 4) "!\n"
+                       "She swallowed the " (nth animals 4) " to catch the " (nth animals 3) ",\n"
+                       "She swallowed the " (nth animals 3) " to catch the " (nth animals 2) ",\n"
+                       "She swallowed the " (nth animals 2) " to catch the " (second animals) ",\n"
+                       "She swallowed the " (second animals) " to catch the " (first animals) ";\n"
+                       "I don't know why she swallowed a " (first animals) " - perhaps she'll die!\n\n"
+                       "There was an old lady who swallowed a " (nth animals 5) ";\n"
+                       "I don't know how she swallowed a " (nth animals 5) "!\n"
+                       "She swallowed the " (nth animals 5) " to catch the " (nth animals 4) ",\n"
+                       "She swallowed the " (nth animals 4) " to catch the " (nth animals 3) ",\n"
+                       "She swallowed the " (nth animals 3) " to catch the " (nth animals 2) ",\n"
+                       "She swallowed the " (nth animals 2) " to catch the " (second animals) ",\n"
+                       "She swallowed the " (second animals) " to catch the " (first animals) ";\n"
+                       "I don't know why she swallowed a " (first animals) " - perhaps she'll die!\n\n"
+                       "There was an old lady who swallowed a " (nth animals 6) "...\n"
+                       "...She's dead, of course!\n"))
 
 (defn sing [& args]
   default-song)
