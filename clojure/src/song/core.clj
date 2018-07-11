6d112056ffd07f4c77e478d1807fcd9cd2990c7a
thisdotrob <rs@gower.st>
2018-07-11 12:52:29 +0100

    Move remainng comments to vec

--- clojure/src/song/core.clj
+++ clojure/src/song/core.clj
@@ -4,34 +4,40 @@
 
 (def animals ["fly" "spider" "bird" "cat" "dog" "cow" "horse"])
 
-(def comments ["I don't know why she swallowed a fly - perhaps she'll die!"])
+(def comments ["I don't know why she swallowed a fly - perhaps she'll die!"
+               "That wriggled and wiggled and tickled inside her."
+               "How absurd to swallow a bird."
+               "Fancy that to swallow a cat!"
+               "What a hog, to swallow a dog!"
+               "I don't know how she swallowed a cow!"
+               "...She's dead, of course!"])
 
 (def default-song (str "There was an old lady who swallowed a " (first animals) ".\n"
                        (first comments) "\n\n"
                        "There was an old lady who swallowed a " (second animals) ";\n"
-                       "That wriggled and wiggled and tickled inside her.\n"
+                       (second comments) "\n"
                        "She swallowed the " (second animals) " to catch the fly;\n"
                        (first comments) "\n\n"
                        "There was an old lady who swallowed a " (nth animals 2) ";\n"
-                       "How absurd to swallow a " (nth animals 2) ".\n"
+                       (nth comments 2) "\n"
                        "She swallowed the " (nth animals 2) " to catch the " (second animals) ",\n"
                        "She swallowed the " (second animals) " to catch the " (first animals) ";\n"
                        (first comments) "\n\n"
                        "There was an old lady who swallowed a " (nth animals 3) ";\n"
-                       "Fancy that to swallow a " (nth animals 3) "!\n"
+                       (nth comments 3) "\n"
                        "She swallowed the " (nth animals 3) " to catch the " (nth animals 2) ",\n"
                        "She swallowed the " (nth animals 2) " to catch the " (second animals) ",\n"
                        "She swallowed the " (second animals) " to catch the " (first animals) ";\n"
                        (first comments) "\n\n"
                        "There was an old lady who swallowed a " (nth animals 4) ";\n"
-                       "What a hog, to swallow a " (nth animals 4) "!\n"
+                       (nth comments 4) "\n"
                        "She swallowed the " (nth animals 4) " to catch the " (nth animals 3) ",\n"
                        "She swallowed the " (nth animals 3) " to catch the " (nth animals 2) ",\n"
                        "She swallowed the " (nth animals 2) " to catch the " (second animals) ",\n"
                        "She swallowed the " (second animals) " to catch the " (first animals) ";\n"
                        (first comments) "\n\n"
                        "There was an old lady who swallowed a " (nth animals 5) ";\n"
-                       "I don't know how she swallowed a " (nth animals 5) "!\n"
+                       (nth comments 5) "\n"
                        "She swallowed the " (nth animals 5) " to catch the " (nth animals 4) ",\n"
                        "She swallowed the " (nth animals 4) " to catch the " (nth animals 3) ",\n"
                        "She swallowed the " (nth animals 3) " to catch the " (nth animals 2) ",\n"
@@ -39,7 +45,7 @@
                        "She swallowed the " (second animals) " to catch the " (first animals) ";\n"
                        (first comments) "\n\n"
                        "There was an old lady who swallowed a " (nth animals 6) "...\n"
-                       "...She's dead, of course!\n"))
+                       (nth comments 6) "\n"))
 
 (defn sing [& args]
   default-song)
