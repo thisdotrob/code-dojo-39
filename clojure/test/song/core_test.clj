387d44c1d18d2c57894a3e438106c7a8e9d4648c
thisdotrob <rs@gower.st>
2018-07-12 15:03:13 +0100

    Add test for verse function

--- clojure/test/song/core_test.clj
+++ clojure/test/song/core_test.clj
@@ -2,6 +2,30 @@
   (:require [clojure.test :refer :all]
             [song.core :as sut]))
 
+(deftest animals->verse
+  (testing "It returns a verse for one animal"
+    (let [animals ["turtle"]
+          comments ["Something about a turtle!"]
+          expected "There was an old lady who swallowed a turtle.\nSomething about a turtle!\n\n"]
+      (is (= expected (sut/animals-&-comments->verse animals comments)))))
+  (testing "It returns a verse for two animals"
+    (let [animals ["turtle" "dog"]
+          comments ["Something about a turtle!" "Line about the dog..."]
+          expected (str "There was an old lady who swallowed a dog;\n"
+                        "Line about the dog...\n"
+                        "She swallowed the dog to catch the turtle;\n"
+                        "Something about a turtle!\nn")]
+      (is (= expected (sut/animals-&-comments->verse animals comments)))))
+  (testing "It returns a verse for three animals"
+    (let [animals ["turtle" "dog" "penguin"]
+          comments ["Something about a turtle!" "Line about the dog..." "Penguin related stuff-"]
+          expected (str "There was an old lady who swallowed a penguin;\n"
+                        "Penguin related stuff-\n"
+                        "She swallowed the penguin to catch the dog,\n"
+                        "She swallowed the dog to catch the turtle;\n"
+                        "Something about a turtle!\nn")]
+      (is (= expected (sut/animals-&-comments->verse animals comments))))))
+
 (deftest animals->swallowed-catched-line
   (testing "It returns a swallowed ... to catch ... line"
     (let [animals ["cat" "dog"]
